d557d3a2b491fdad6d4feb96635406679367d9af
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-05-24 11:49:30 +0200

    Initialize empty Android Studio project

---
+++ app/src/main/java/com/github/kaeptmblaubaer1000/smalipatchgenerator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.kaeptmblaubaer1000.smalipatchgenerator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
