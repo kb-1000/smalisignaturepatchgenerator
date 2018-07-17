5fd5d755569265b07eeb3216a06a4b3c31575bab
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-07-17 13:35:42 +0200

    Also generate SignatureVerificationTypes

--- buildSrc/src/main/java/PatchDefCompiler.kt
+++ buildSrc/src/main/java/PatchDefCompiler.kt
@@ -1,12 +1,26 @@
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import java.io.File
 
 object PatchDefCompiler {
     @JvmStatic
     fun generate(file: File) {
+        val signatureVerificationTypes = listOf("firstPatch")
+        val signatureVerificationTypesClassConstructor = FunSpec.constructorBuilder()
+                .addParameters(signatureVerificationTypes.map { ParameterSpec.builder(it, Boolean::class).defaultValue("false").build() })
+
+        val signatureVerificationTypesClass = TypeSpec.classBuilder(ClassName("com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated", "SignatureVerificationTypes"))
+                .primaryConstructor(signatureVerificationTypesClassConstructor
+                        .build())
+                .addModifiers(KModifier.DATA)
+                .addProperties(signatureVerificationTypes.map { PropertySpec.varBuilder(it, Boolean::class).initializer(it).build() })
+                .build()
+
         val identificationClassDefRewriterClass = TypeSpec.classBuilder(ClassName("com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated", "IdentificationClassDefRewriter"))
                 .superclass(ClassName("org.jf.dexlib2.rewriter", "ClassDefRewriter"))
                 .primaryConstructor(FunSpec.constructorBuilder()
@@ -14,8 +28,11 @@ object PatchDefCompiler {
                         .build())
                 .addSuperclassConstructorParameter("rewriters")
                 .build()
-        val fileSpec = FileSpec.builder("com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated", "IdentificationClassDefRewriter")
+
+
+        val fileSpec = FileSpec.builder("com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated", "CompiledPatchDef")
                 .addType(identificationClassDefRewriterClass)
+                .addType(signatureVerificationTypesClass)
                 .build()
         fileSpec.writeTo(file)
     }
