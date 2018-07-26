3cca67ed82e359da7f6e977ff39b7353bca71713
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-07-26 11:47:35 +0200

    Add modifiedClass to the PatchDef

--- parser/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/patchdefparser/PatchDef.kt
+++ parser/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/patchdefparser/PatchDef.kt
@@ -1,9 +1,9 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.patchdefparser
 
-data class NullablePatchDef(var humanName: String? = null) {
+data class NullablePatchDef(var humanName: String? = null, var modifiedClass: String? = null) {
     fun toNonNullable(): PatchDef? {
-        return PatchDef(humanName = humanName ?: return null)
+        return PatchDef(humanName = humanName ?: return null, modifiedClass = modifiedClass ?: return null)
     }
 }
 
-data class PatchDef(val humanName: String)
+data class PatchDef(val humanName: String, val modifiedClass: String)
