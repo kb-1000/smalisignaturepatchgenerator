8d2dda852f5c502e268785baec5c9b23f614c5ca
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-20 09:11:53 +0200

    Also generate NullablePatchDef

--- parser/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/patchdefparser/PatchDef.kt
+++ parser/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/patchdefparser/PatchDef.kt
@@ -1,19 +1,5 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.patchdefparser
 
-data class NullablePatchDef(
-        var humanName: String? = null,
-        var modifiedClass: String? = null,
-        var modifiedMethod: String? = null
-) {
-    fun toNonNullable(): PatchDef? {
-        return PatchDef(
-                humanName = humanName ?: return null,
-                modifiedClass = modifiedClass ?: return null,
-                modifiedMethod = modifiedMethod ?: return null
-        )
-    }
-}
-
 data class PatchDef(
         val humanName: String,
         val modifiedClass: String,
