35f261b0745b5a9614c107e02f2fe50d41f25c83
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-23 11:27:14 +0200

    Actually implement main

--- main/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/Main.kt
+++ main/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/Main.kt
@@ -4,9 +4,23 @@ import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.mainlib.IMain
 
 class Main {
     companion object : IMain {
+        val mains: Map<String, IMain> = mapOf(
+                "main" to Main
+        )
+
         @JvmStatic
         override fun main(vararg args: String) {
+            if (args.isEmpty()) {
+                println("Please give a command name as the first parameter to this program.")
+                printCommandNames()
+            }
+        }
 
+        private fun printCommandNames() {
+            println("Available commands:")
+            for((name, command) in mains) {
+                println("\t$name")
+            }
         }
     }
 }
