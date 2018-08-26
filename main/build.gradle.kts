71803e20ada600072ad8104a7660cb1543b4ff26
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-26 18:29:48 +0200

    Prepare usage of ProGuard

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -5,14 +5,20 @@ plugins {
 
 val kotlin_version: String by rootProject.extra
 
+tasks.withType(CreateStartScripts::class.java).getByName("startScripts") {
+    //classpath = files("lib/SmaliSignaturePatchGenerator.jar") //TODO: include this after adding ProGuard
+}
+
 application {
     mainClassName = "com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.Main"
+    applicationName = "smali-signature-patch-generator"
+
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation(project(":mainlib"))
 
-    
+
     implementation(project(":cli"))
 }
