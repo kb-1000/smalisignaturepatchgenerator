2bdcf8bc5082c54ba61a51a9ea66756f30caa2f5
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-27 18:39:16 +0200

    Fix API of PatchGenerator.identifySignatureVerificationTypes

--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/DexFileWrapper.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/DexFileWrapper.kt
@@ -2,4 +2,7 @@ package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
 
 import org.jf.dexlib2.iface.DexFile
 
-data class DexFileWrapper(val dexFile: DexFile)
+data class DexFileWrapper(val dexFile: DexFile) : DexFile by dexFile
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun DexFile.wrap() = DexFileWrapper(this)
--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/PatchGenerator.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/PatchGenerator.kt
@@ -2,16 +2,16 @@ package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
 
 import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated.SignatureVerificationTypes
 import org.jf.dexlib2.dexbacked.DexBackedDexFile
-import org.jf.dexlib2.iface.DexFile
 import org.jf.dexlib2.rewriter.DexRewriter
 import java.io.File
 import java.util.zip.ZipFile
 
 class PatchGenerator {
-    fun identifySignatureVerificationTypes(dexFile: DexFile): Pair<DexFile, SignatureVerificationTypes> {
+    fun identifySignatureVerificationTypes(dexFile: DexFileWrapper): Pair<DexFileWrapper, SignatureVerificationTypes> {
         val signatureVerificationTypes = SignatureVerificationTypes()
-        val rebuiltDexFile = rebuildDexFile(DexRewriter(IdentificationRewriterModule(signatureVerificationTypes)).rewriteDexFile(dexFile))
-        return Pair(rebuiltDexFile, signatureVerificationTypes)
+        val rebuiltDexFile = rebuildDexFile(DexRewriter(IdentificationRewriterModule(signatureVerificationTypes)).rewriteDexFile(dexFile.dexFile))
+        return Pair(rebuiltDexFile.wrap(), signatureVerificationTypes)
+
     }
 
     @Throws(InvalidApkFileException::class)
