f813b896d56862fbc84ce3d8488ee7ba90a56032
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-27 10:54:36 +0200

    Fix the identification of the signature verification types

--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/Functions.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/Functions.kt
@@ -1,11 +1,21 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
 
 import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated.SignatureVerificationTypes
+import org.jf.dexlib2.dexbacked.DexBackedDexFile
 import org.jf.dexlib2.iface.DexFile
 import org.jf.dexlib2.rewriter.DexRewriter
+import org.jf.dexlib2.writer.io.MemoryDataStore
+import org.jf.dexlib2.writer.pool.DexPool
+import java.io.ByteArrayInputStream
 
 fun identifySignatureVerificationTypes(dexFile: DexFile): Pair<DexFile, SignatureVerificationTypes> {
     val signatureVerificationTypes = SignatureVerificationTypes()
-    val rebuiltDexFile = DexRewriter(IdentificationRewriterModule(signatureVerificationTypes)).rewriteDexFile(dexFile)
+    val rebuiltDexFile = rebuildDexFile(DexRewriter(IdentificationRewriterModule(signatureVerificationTypes)).rewriteDexFile(dexFile))
     return Pair(rebuiltDexFile, signatureVerificationTypes)
+}
+
+fun rebuildDexFile(dexFile: DexFile): DexFile {
+    val dataStore = MemoryDataStore()
+    DexPool.writeTo(dataStore, dexFile)
+    return DexBackedDexFile.fromInputStream(dexFile.opcodes, ByteArrayInputStream(dataStore.data))
 }
