b498dcaecffa20a7169e90b68ddaf12006e30653
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-08-27 15:06:50 +0200

    Implement DEX loading in cli.Main

--- cli/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/cli/Main.kt
+++ cli/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/cli/Main.kt
@@ -1,11 +1,19 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.cli
 
+import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.PatchGenerator
 import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.mainlib.IMain
+import java.io.File
 
 class Main {
     companion object : IMain {
         @JvmStatic
         override fun main(args: Array<String>) {
+            if (args.size != 2) {
+                //TODO: print usage
+            } else {
+                val patchGenerator = PatchGenerator()
+                val dexFile = patchGenerator.loadApkFile(File(args[0]))
+            }
         }
 
         override val description: String
--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/PatchGenerator.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/PatchGenerator.kt
@@ -1,13 +1,34 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
 
 import com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core.generated.SignatureVerificationTypes
+import org.jf.dexlib2.dexbacked.DexBackedDexFile
 import org.jf.dexlib2.iface.DexFile
 import org.jf.dexlib2.rewriter.DexRewriter
+import java.io.File
+import java.util.zip.ZipFile
 
 class PatchGenerator {
     fun identifySignatureVerificationTypes(dexFile: DexFile): Pair<DexFile, SignatureVerificationTypes> {
         val signatureVerificationTypes = SignatureVerificationTypes()
         val rebuiltDexFile = rebuildDexFile(DexRewriter(IdentificationRewriterModule(signatureVerificationTypes)).rewriteDexFile(dexFile))
         return Pair(rebuiltDexFile, signatureVerificationTypes)
+    }
+
+    @Throws(InvalidApkFileException::class)
+    fun loadApkFile(file: File): DexFile {
+        val zipFile = java.util.zip.ZipFile(file, ZipFile.OPEN_READ) //TODO: catch errors
+        val zipEntry = zipFile.getEntry("classes.dex")
+                ?: throw InvalidApkFileException("The APK file is invalid because it has no classes.dex.")
+        val inputStream = zipFile.getInputStream(zipEntry)
+        return DexBackedDexFile.fromInputStream(null, inputStream)
+    }
+
+
+    @Suppress("UNUSED") // The additional constructors should be there once they are used.
+    class InvalidApkFileException : Exception {
+        constructor() : super()
+        constructor(message: String) : super(message)
+        constructor(message: String, cause: Throwable) : super(message, cause)
+        constructor(cause: Throwable) : super(cause)
     }
 }
