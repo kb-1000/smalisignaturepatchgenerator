94cd9213bc65e8ff599d2536f74fd5ba671f0be4
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-09-07 17:52:40 +0200

    Remove the unused DexFileWrapper

--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/DexFileWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
-
-import org.jf.dexlib2.iface.DexFile
-
-data class DexFileWrapper(val dexFile: DexFile) : DexFile by dexFile
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun DexFile.wrap() = DexFileWrapper(this)
