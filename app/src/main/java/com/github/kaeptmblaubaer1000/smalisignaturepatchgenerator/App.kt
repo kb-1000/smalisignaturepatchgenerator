ac60bd0f0236726a7681ef92ce5a3b32683c5c9a
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-09-30 08:39:03 +0200

    Migrate to AndroidX

--- app/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/App.kt
+++ app/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/App.kt
@@ -2,7 +2,7 @@ package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator
 
 import android.app.Application
 import android.content.Context
-import android.support.multidex.MultiDex
+import androidx.multidex.MultiDex
 
 class App : Application() {
     override fun attachBaseContext(base: Context) {
