11842aedda5c618743e6d8e36bd3927cc5e1435d
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-10-23 17:09:53 +0200

    Update Kotlin to 1.2.71

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.extra["kotlin_version"] = "1.2.61"
+rootProject.extra["kotlin_version"] = "1.2.71"
 rootProject.extra["antlr_version"] = "4.7.1"
 rootProject.extra["smaliVersion"] = "2.2.5"
 rootProject.extra["kotlinPoetVersion"] = "0.7.0"
