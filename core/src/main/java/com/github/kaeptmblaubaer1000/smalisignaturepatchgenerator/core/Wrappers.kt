6e7c93dd0ffcb39b0366666c77cd46ed1fb39366
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-10-29 18:56:34 +0100

    Complete the wrappers

--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/Wrappers.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/Wrappers.kt
@@ -1,28 +1,45 @@
 package com.github.kaeptmblaubaer1000.smalisignaturepatchgenerator.core
 
-import org.jf.dexlib2.iface.instruction.Instruction
+import org.jf.dexlib2.iface.instruction.*
+import org.jf.dexlib2.iface.instruction.formats.*
 import org.jf.dexlib2.iface.Method
 import org.jf.dexlib2.iface.MethodImplementation
+import org.jf.dexlib2.ReferenceType
 
-
-interface Wrapped<T> {
-    fun unwrap(): T
+abstract class Wrapped<T>(val methodName: String) {
+    abstract fun unwrap(): T
 }
 
-class WrappedMethod(private val method: Method) : Method by method, Wrapped<Method> {
+class WrappedMethod(private val method: Method) : Method by method, Wrapped<Method>(method.toString()) {
     override fun getImplementation(): MethodImplementation? {
-        return WrappedMethodImplementation(method.implementation ?: return null)
+        return WrappedMethodImplementation(method.implementation ?: return null, methodName)
     }
 
     override fun unwrap() = method
 }
 
-class WrappedMethodImplementation(private val methodImplementation: MethodImplementation) : MethodImplementation by methodImplementation, Wrapped<MethodImplementation> {
+class WrappedMethodImplementation(private val methodImplementation: MethodImplementation, methodName: String) : MethodImplementation by methodImplementation, Wrapped<MethodImplementation>(methodName) {
     override fun unwrap() = methodImplementation
 
     override fun getInstructions(): Iterable<Instruction> = methodImplementation.instructions.map {
         when(it) {
+            is ReferenceInstruction -> when(it.referenceType) {
+	        ReferenceType.METHOD -> when(it) {
+		    is Instruction35c -> WrappedInstruction35c(it, methodName)
+		    is Instruction3rc -> WrappedInstruction3rc(it, methodName)
+		    else -> it
+		}
+		else -> it
+	    }
             else -> it
         }
     }
+}
+
+class WrappedInstruction35c(private val instruction35c: Instruction35c, methodName: String) : Wrapped<Instruction35c>(methodName), Instruction35c by instruction35c {
+    override fun unwrap() = instruction35c
+}
+
+class WrappedInstruction3rc(private val instruction3rc: Instruction3rc, methodName: String) : Wrapped<Instruction3rc>(methodName), Instruction3rc by instruction3rc {
+    override fun unwrap() = instruction3rc
 }
