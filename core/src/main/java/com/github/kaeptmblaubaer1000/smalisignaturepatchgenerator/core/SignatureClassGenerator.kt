5a589e976f43e9e88a891a70e5868364471c6fa6
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-12-03 07:47:34 +0100

    Add and initialize field to store emulated Signatures

--- core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/SignatureClassGenerator.kt
+++ core/src/main/java/com/github/kaeptmblaubaer1000/smalisignaturepatchgenerator/core/SignatureClassGenerator.kt
@@ -4,6 +4,7 @@ import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 import org.jf.dexlib2.ReferenceType
 import org.jf.dexlib2.base.reference.BaseMethodReference
+import org.jf.dexlib2.base.reference.BaseFieldReference
 import org.jf.dexlib2.base.reference.BaseTypeReference
 import org.jf.dexlib2.iface.Annotation
 import org.jf.dexlib2.iface.ClassDef
@@ -17,8 +18,13 @@ import org.jf.dexlib2.iface.debug.DebugItem
 import org.jf.dexlib2.iface.instruction.FiveRegisterInstruction
 import org.jf.dexlib2.iface.instruction.Instruction
 import org.jf.dexlib2.iface.instruction.formats.Instruction10x
+import org.jf.dexlib2.iface.instruction.formats.Instruction11n
+import org.jf.dexlib2.iface.instruction.formats.Instruction21c
+import org.jf.dexlib2.iface.instruction.formats.Instruction22c
 import org.jf.dexlib2.iface.instruction.formats.Instruction35c
 import org.jf.dexlib2.iface.reference.Reference
+import org.jf.dexlib2.iface.reference.TypeReference
+import org.jf.dexlib2.iface.value.EncodedValue
 import java.math.BigInteger
 import java.security.MessageDigest
 
@@ -26,12 +32,21 @@ private val emptyList: List<Nothing> = emptyList()
 private val emptySet: Set<Nothing> = emptySet()
 
 fun generateSignatureClass(signatureData: List<String>): ClassDef {
+    if (signatureData.size > 7) {
+        //Because every application where I counted only had one Signature, this is a reasonable limit that saves space in the resulting DEX file.
+        throw UnsupportedOperationException("It's currently impossible to use more than seven Signatures")
+    }
     return object : BaseTypeReference(), ClassDef {
         private val type: String = run {
             val md = MessageDigest.getInstance("SHA-512")
             md.update(signatureData.joinTo(StringBuilder(), "\n").toString().toByteArray(Charsets.US_ASCII))
             val digest = md.digest()
             "Signature${BigInteger(digest).toString(16).padStart(digest.size * 2, '0')}"
+        }
+
+        private inline fun className(function: () -> Unit): String {
+            function()
+            return type
         }
 
         abstract inner class BaseMethod : BaseMethodReference(), Method {
@@ -49,6 +64,11 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
 
         abstract inner class BaseInstruction : Instruction {
             override fun getCodeUnits() = opcode.format.size / 2
+        }
+
+        abstract inner class BaseConst4Instruction : BaseInstruction(), Instruction11n {
+            override fun getOpcode() = Opcode.CONST_4
+            override fun getWideLiteral() = narrowLiteral.toLong()
         }
 
         abstract inner class BaseFiveRegisterInstruction : BaseInstruction(), FiveRegisterInstruction {
@@ -69,6 +89,10 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
             override fun getOpcode() = Opcode.RETURN_VOID
         }
 
+        private val signatureArrayTypeReference: TypeReference = object : BaseTypeReference() {
+            override fun getType() = "Landroid/content/pm/Signature;"
+        }
+
         override fun getType() = type
 
         override fun getAnnotations(): Set<Annotation> = emptySet
@@ -82,7 +106,7 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
             private val implementation: MethodImplementation = object : BaseMethodImplementation() {
                 override fun getRegisterCount() = 1
 
-                private val instructions: Iterable<Instruction> = listOf(object : BaseInvokeDirectInstruction(){
+                private val instructions: Iterable<Instruction> = listOf(object : BaseInvokeDirectInstruction() {
                     override fun getRegisterCount() = 1
                     override fun getRegisterC() = 0
 
@@ -96,6 +120,7 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
 
                     override fun getReference(): Reference = reference
                 }, returnVoidInstruction)
+
                 override fun getInstructions() = instructions
             }
 
@@ -106,7 +131,34 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
 
             private val implementation: MethodImplementation = object : BaseMethodImplementation() {
                 override fun getRegisterCount() = 3
-                private val instructions: Iterable<Instruction> = listOf()
+                private val instructions: Iterable<Instruction> = listOf(object : BaseConst4Instruction() {
+                    override fun getNarrowLiteral() = signatureData.size
+                    override fun getRegisterA() = 0
+                }, object : BaseInstruction(), Instruction22c {
+                    override fun getOpcode() = Opcode.NEW_ARRAY
+                    override fun getRegisterA() = 0
+                    override fun getRegisterB() = 0
+                    override fun getReferenceType() = ReferenceType.TYPE
+                    private val reference: Reference = signatureArrayTypeReference
+                    override fun getReference() = reference
+                }, *signatureData.fold<String, ArrayList<Instruction>>(ArrayList<Instruction>(signatureData.size * 4), { arrayList: ArrayList<Instruction>, s: String ->
+                    arrayList.add(object : BaseInstruction(), Instruction21c {
+                        override fun getOpcode() = Opcode.NEW_INSTANCE
+                        private val reference: Reference = object : BaseTypeReference() {
+                            override fun getType() = "Landroid/content/pm/Signature;"
+                        }
+                        override fun getReference() = reference
+                        override fun getReferenceType() = ReferenceType.TYPE
+                        override fun getRegisterA() = 1
+                    })
+                    arrayList
+                }).toTypedArray(), object : BaseInstruction(), Instruction21c {
+                    override fun getOpcode() = Opcode.SPUT_OBJECT
+                    private val reference: Reference = getListStaticFields({})[0]
+                    override fun getReference() = reference
+                    override fun getReferenceType() = ReferenceType.FIELD
+                    override fun getRegisterA() = 0
+                })
 
                 override fun getInstructions() = instructions
             }
@@ -120,9 +172,23 @@ fun generateSignatureClass(signatureData: List<String>): ClassDef {
 
         override fun getSuperclass(): String? = null
 
-        private val staticFields: Iterable<Field> = listOf()
+        private val staticFields = listOf(object : BaseFieldReference(), Field {
+            override fun getDefiningClass() = className {}
+            override fun getName() = "signatures"
+            private val type = signatureArrayTypeReference.toString()
+            override fun getType() = type
+            private val accessFlags = AccessFlags.STATIC.ordinal or AccessFlags.PUBLIC.ordinal or AccessFlags.FINAL.ordinal
+            override fun getAccessFlags() = accessFlags
+            override fun getInitialValue(): EncodedValue? = null
+            override fun getAnnotations(): Set<Annotation> = emptySet
+        })
 
-        override fun getStaticFields() = staticFields
+        private inline fun getListStaticFields(function: () -> Unit): List<Field> {
+            function()
+            return staticFields
+        }
+
+        override fun getStaticFields(): Iterable<Field> = staticFields
 
         override fun getInstanceFields(): Iterable<Field> = emptyList
 
